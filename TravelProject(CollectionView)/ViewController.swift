b5a994106202dbf1cc220b6a54e3160a7bbc8934
a-jb97 <a_jb97@naver.com>
2026-01-14 16:32:49 +0900

    Initial Commit

---
+++ TravelProject(CollectionView)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelProject(CollectionView)
+//
+//  Created by 전민돌 on 1/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
