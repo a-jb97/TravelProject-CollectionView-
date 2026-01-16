90543d675397fe58b2c0d3b9e12a60b7d3f068c5
a-jb97 <a_jb97@naver.com>
2026-01-16 19:58:31 +0900

    Design : ChatListCollectionViewCell(채팅방 목록 셀) UI 및 레이아웃 설정

---
+++ TravelProject(CollectionView)/TravelTalk/ChatListCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ChatListCollectionViewCell.swift
+//  TravelProject(CollectionView)
+//
+//  Created by 전민돌 on 1/16/26.
+//
+
+import UIKit
+import Kingfisher
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ChatListCollectionViewCell"
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var timeStampLabel: UILabel!
+    
+    func configUI(row: ChatRoom) {
+        let user = mockUsers[row.participantIds[1]]
+        
+        profileImageView.kf.setImage(with: URL(string: user.profileImage))
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        
+        userNameLabel.text = user.userName
+        contentLabel.text = row.messages.last?.content
+        timeStampLabel.text = setDateFormat(row.messages.last?.timestamp ?? Date())
+    }
+}
+
+extension ChatListCollectionViewCell {
+    // MARK: DateFormat
+    private func setDateFormat(_ date: Date) -> String {
+        let format = DateFormatter()
+        
+        format.dateFormat = "yy.MM.dd"
+        
+        let result = format.string(from: date)
+        
+        return result
+    }
+}
---
+++ TravelProject(CollectionView)/TravelTalk/ChatListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListViewController.swift
+//  TravelProject(CollectionView)
+//
+//  Created by 전민돌 on 1/16/26.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+}
